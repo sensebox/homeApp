a653e6a4a9ca7c907ec49a882405fec363771ea1
Thiemann96 <e_thie10@uni-muenster.de>
2020-07-10 15:04:45 +0200

    added checkbox for remembering login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -14,7 +14,8 @@ export class LoginPage implements OnInit {
   private loginInformation;
   private boxes;
   private saveCredentialsCheckbox: boolean
-  private image = "assets/sensebox_wort_logo.svg"
+  private keepSignedIn:boolean
+
   constructor(
     private LoginService: LoginService,
     private router: Router,
